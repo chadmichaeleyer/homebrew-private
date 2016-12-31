2d3ebbf90134376f1a37817550df4fa5926cea31
chadmichaeleyer <eyer@eyer.us>
2016-12-31 12:11:41 -0500

    added Hypatia Sans Pro

---
+++ private-font-hypatia-sans-pro.rb
@@ -0,0 +1,14 @@
+cask 'private-font-hypatia-sans-pro' do
+  version :latest
+  sha256 :no_check
+
+  url "http://sw.lab.eyer.us/HypatiaSansPro.zip"
+  name 'Hypatia Sans Pro'
+  homepage 'https://athena.eyer.us'
+
+  font "Hypatia Sans Pro.otf"
+  font "Hypatia Sans Pro Bold.otf"
+  font "Hypatia Sans Pro Italic.otf"
+  font "Hypatia Sans Pro Bold Italic.otf"
+    
+end
