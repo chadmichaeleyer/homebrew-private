80154e6d4fbf01a35b8cb2a292a6f0b90e1f23fb
Chad Michael Eyer <eyer@apollo-2.local>
2017-01-07 14:45:12 -0500

    Revised Fonts

--- Casks/font-gentium.rb
+++ Casks/font-gentium.rb
@@ -2,8 +2,7 @@ cask 'font-gentium' do
   version '5.000'
   sha256 :no_check
 
-  url "http://software.sil.org/downloads/d/gentium/GentiumPlus-{version}.zip"
-  
+  url "http://software.sil.org/downloads/d/gentium/GentiumPlus-#{version}.zip"
   name 'Gentium Plus'
   homepage 'http://software.sil.org/gentium/'
 
--- Casks/font-scheherazade.rb
+++ Casks/font-scheherazade.rb
@@ -2,7 +2,7 @@ cask 'font-scheherazade' do
   version '2.100'
   sha256 :no_check
 
-  url "http://software.sil.org/downloads/d/scheherazade/Scheherazade-{version}.zip"
+  url "http://software.sil.org/downloads/d/scheherazade/Scheherazade-#{version}.zip"
   name 'Scheherazade'
   homepage 'http://software.sil.org/scheherazade/'
 
