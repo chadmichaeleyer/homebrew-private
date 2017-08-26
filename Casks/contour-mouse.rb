aa7a3e3a660819953b499f2bbab2597553ce9394
Chad Michael Eyer <eyer@users.noreply.github.com>
2017-08-26 10:50:59 -0400

    Update contour-mouse.rb

--- Casks/contour-mouse.rb
+++ Casks/contour-mouse.rb
@@ -2,7 +2,7 @@ cask 'contour-mouse' do
   version '3.1.2'
   sha256 :no_check
 
-  url "http://www.contourdesign.com/US/wp-content/uploads/2017/04/Contour-Mouse-#{version}.zip"
+  url "http://sw.lab.eyer.us/Contour-Mouse-#{version}.zip"
   name 'Contour Design'
   homepage 'http://www.contourdesign.com/US/support'
 
