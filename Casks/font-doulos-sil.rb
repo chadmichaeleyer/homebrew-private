65b219bc3ca82ee9baba51c7e95aa98ae946ddf1
Chad Michael Eyer <eyer@users.noreply.github.com>
2017-08-26 10:51:20 -0400

    Update font-doulos-sil.rb

--- Casks/font-doulos-sil.rb
+++ Casks/font-doulos-sil.rb
@@ -2,7 +2,7 @@ cask 'font-doulos-sil' do
   version '5.000'
   sha256 '0b309c3db813a98ce884c0bd25c7f5c0bd96bbffd076459e39298812ca22472e'
 
-  url "http://software.sil.org/downloads/d/doulos/DoulosSIL-#{version}.zip"
+  url "http://sw.lab.eyer.us/DoulosSIL-#{version}.zip"
   name 'Doulos SIL'
   homepage 'https://www.sil.org/resources/software_fonts/doulos-sil'
 
