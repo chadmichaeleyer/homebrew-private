794f1a4cd03a052fe798e0b1219aa302542628e0
Chad Michael Eyer <eyer@users.noreply.github.com>
2017-08-26 10:19:34 -0400

    Update contour-mouse.rb

--- Casks/contour-mouse.rb
+++ Casks/contour-mouse.rb
@@ -1,12 +1,12 @@
 cask 'contour-mouse' do
-  version '3.1.0'
-  sha256 '324e1ae893722d99dea7737341f841bf46f270a76fc85436aa77f3cf49cd9043'
+  version '3.1.2'
+  sha256 :nocheck
 
-  url "http://www.contourdesign.com/US/wp-content/uploads/2016/08/Contour-Mouse-#{version}-1.zip"
+  url "http://www.contourdesign.com/US/wp-content/uploads/2017/04/Contour-Mouse-#{version}.zip"
   name 'Contour Design'
   homepage 'http://www.contourdesign.com/US/support'
 
-  container nested: (nested_container = "Contour Mouse #{version} (1).dmg")
+  container nested: (nested_container = "Contour Mouse #{version}.dmg")
   
   pkg "Contour Mouse.pkg"
 
